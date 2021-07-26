8dc369662163a33290063832009fe85daab85456
coopsx <coopsx@googlemail.com>
2021-07-26 12:24:41 +0000

    added template and subsicquent html pages

--- ms3.py
+++ ms3.py
@@ -1,16 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'name': 'Tom Jackson',
+        'title': 'Introduction',
+        'content': 'Introductory Post',
+        'date': '26 JUNE, 2021'
+    },
+    {
+        'name': 'James Cooper',
+        'title': 'Barolo',
+        'content': 'Barolo Wine Review',
+        'date': '28 JUNE, 2021'
+    }
+]
+
 
 @app.route("/")
+@app.route("/index")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
 
 @app.route("/about")
 def about():
-    return "<p>About, World!</p>"
+    return render_template('about.html', title='About')
+
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html', title='Blog', posts=posts)
 
 
 if __name__ == '__main__':
