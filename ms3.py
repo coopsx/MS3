f27334ee4920ecd1dee8e06346ed8a93da17004b
coopsx <coopsx@googlemail.com>
2021-07-27 10:42:44 +0000

    fixed log in bugs

--- ms3.py
+++ ms3.py
@@ -37,10 +37,17 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@wineblog.com' and form.password.data == 'password':
+            flash('Login Successful.', 'success')
+            return redirect(url_for('login'))s
+        else: 
+            flash('Login Unsuccessful. Please check details and try again', 'danger')
     return render_template('login.html', title='Login', form=form)
+
 
 
 @app.route("/about")
